99e3a81fda26f538b1b2fb187ffcda31a4d8f8a8
Mahdi Bolow <boolow5@gmail.com>
2016-03-18 02:46:58 +0300

    chatting is ok, but notification is underway

--- client/templates/home/users.js
+++ client/templates/home/users.js
@@ -8,12 +8,24 @@ Template.allUsers.helpers({
 Template.onlineUsers.helpers({
 	'onlineUsers': function() {
 		Meteor.subscribe('onlineUsers');
-	  return Meteor.users.find({ "status.online": true }).fetch();
+	  return Meteor.users.find({ "status.online": true, _id: {$ne: Meteor.userId()} }).fetch();
+	}, 
+	'notes': function () {
+		Meteor.subscribe('notes');
+		return Notes.find().fetch();
+	}
+});
+
+Template.onlineUsers.events({
+	'click span.deleteNote': function (event) {
+		console.log('deleting a new note');
 	}
 });
 
 Template.chatWithUser.helpers({
 	'data': function () {
+		receiver = Router.current().params.userID
+		
 		return {
 			'messages': function () {
 				Meteor.subscribe('messages');
@@ -35,5 +47,9 @@ Template.chatWithUser.events({
 		Meteor.call('sendMessage', {'receiver':receiver, 'message':message});
 		console.log("Message: "+message+"\nReceiver: "+receiver);
 		console.log(Router.current().params.userID);
+		
+		$("#display-for-"+receiver).animate({ scrollTop: $("#display-for-"+receiver)[0].scrollHeight}, 1000);
+
 	}
 });
+
--- server/methods.js
+++ server/methods.js
@@ -5,7 +5,12 @@ Meteor.methods({
 		var message = mObj.message;
 		
 		Messages.insert({'message': message, 'sender':sender, 'receiver':receiver, timeSent:new Date()});
+		
+		Notes.insert({to: receiver, from: sender, title: "Your received a new message "});
 		console.log("message sent from server");
 		console.log(Messages.find().count());
+	},
+	'deleteNotice': function (noteId) {
+		Notes.delete({_id:noteId});
 	}
 });
